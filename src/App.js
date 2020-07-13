32ddb5b1bd44282d4186faa0325e1a988a8d97d9
artikgupta <artig9262@gmail.com>
2020-07-13 14:22:07 +0530

    create todos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
-import React from "react";
-import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <input type="text" placeholder="Search..." />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todoInput: "",
+      todoArray: ["cleaning", "studying"],
+    };
+  }
+
+  onChange = (event) => {
+    this.setState({
+      todoInput: event.target.value,
+    });
+  };
+
+  onClick = () => {
+    const newTodoArray = [...this.state.todoArray];
+    newTodoArray.push(this.state.todoInput);
+    console.log(this.state.todoInput);
+    this.setState({
+      todoInput: "",
+      todoArray: newTodoArray,
+    });
+  };
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          value={this.state.todoInput}
+          onChange={this.onChange}
+        />
+        <button onClick={this.onClick}> click me</button>
+        <div>
+          <h1>
+            {this.state.todoArray.map((todo) => {
+              return <div key={todo}>{todo}</div>;
+            })}
+          </h1>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
